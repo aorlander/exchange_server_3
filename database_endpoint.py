668ee3f986d60618f6fd1a4ba188ac134d347ad5
aorlander <aborlander@gmail.com>
2021-04-01 22:10:13 -0400

    test

--- database_endpoint.py
+++ database_endpoint.py
@@ -29,15 +29,21 @@ def shutdown_session(response_or_exc):
 """
 -------- Helper methods (feel free to add your own!) -------
 """
-
+#If the signature does not verify, do not insert the order into the “Order” table. 
+#Instead, insert a record into the “Log” table, with the message field set to be json.dumps(payload).
 def log_message(d):
-    print(d)
+    time = datetime.now()
+    log = Log(logtime=time, message=d))
+    session.add(log)
+    session.commit()
     pass
 
 """
 ---------------- Endpoints ----------------
 """
-    
+#Accept POST data in JSON format. 
+#Orders should have two fields “payload” and "sig".
+#The payload will contain the following fields, ‘sender_pk’,’receiver_pk,’buy_currency’,’sell_currency’,’buy_amount’,’sell_amount’,’platform’.
 @app.route('/trade', methods=['POST'])
 def trade():
     if request.method == "POST":
@@ -62,15 +68,58 @@ def trade():
             print( json.dumps(content) )
             log_message(content)
             return jsonify( False )
-            
+        
         #Your code here
-        #Note that you can access the database session using g.session
+        s_pk = content['payload']['sender_pk'] 
+        r_pk = content['payload']['receiver_pk'] 
+        buy_ccy = content['payload']['buy_currency'] 
+        sell_ccy = content['payload']['sell_currency'] 
+        buy_amt = content['payload']['buy_amount'] 
+        sell_amt = content['payload']['sell_amount'] 
+        platform = content['payload']['platform']
+        sig = content['sig']
+        payload = json.dumps(content['payload'])
+        response = False
+        if platform=='Ethereum':
+            eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+            if eth_account.Account.recover_message(eth_encoded_msg,signature=sig) == pk:
+                response = True
+        if platform=='Algorand':
+            if algosdk.util.verify_bytes(payload.encode('utf-8'),sig,pk):
+                response = True
 
+        #If the signature verifies, all of the fields under the ‘payload’ key should be stored in the “Order” table EXCEPT for 'platform’.
+        if response == True:
+            order = Order( sender_pk=s_pk, receiver_pk=r_pk, buy_currency=buy_ccy, sell_currency=sell_ccy, buy_amount=buy_amt, sell_amount=sell_amt)
+            session.add(order)
+            session.commit()
+        #If the signature does not verify, do not insert the order into the “Order” table. 
+        #Instead, insert a record into the “Log” table, with the message field set to be json.dumps(payload).
+        if response == False:
+            leg_message(payload)
+
+        return 0
+
+
+
+#Return a list of all orders in the database. The response should be a list of orders formatted as JSON. 
+# Each order should be a dict with (at least) the following fields ("sender_pk", "receiver_pk", 
+# "buy_currency", "sell_currency", "buy_amount", "sell_amount", “signature”).
 @app.route('/order_book')
 def order_book():
-    #Your code here
-    #Note that you can access the database session using g.session
-    result = 0
+    orders = session.query(Order).all()
+    result = []
+    for order in orders:
+        obj = {
+            "sender_pk": order.sender_pk, 
+            "receiver_pk": order.receier_pk, 
+            "buy_currency": order.buy_currency, 
+            "sell_currency": order.sell_currency, 
+            "buy_amount": order.buy_amount, 
+            "sell_amount": order.sell_amount, 
+            "signature": order.signature }
+        json_obj = json.dumps(obj)
+        result.add(json_obj)
     return jsonify(result)
 
 if __name__ == '__main__':
